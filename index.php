4a4e502827423f673feeb8cd7a3f7dd3326d0764
Jerry zhang <sir.zhang@foxmail.com>
2015-08-11 17:08:45 +0800

    up

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 require dirname(__FILE__).'/protected/config/custom.php';
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
-define('SITE_URL','http://106.75.194.11/');
+define('SITE_URL','http://localhost/');
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 require_once($yii);
