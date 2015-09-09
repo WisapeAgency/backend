3f23b25282a2d9d5e7a0645da59a2e2f2c2561c3
zhangjijun <sir.zhang@foxmail.com>
2015-09-09 15:56:03 +0800

    接收digital river数据并入库

--- protected/models/PaymentRecord.php
+++ protected/models/PaymentRecord.php
@@ -1,20 +1,29 @@
 <?php
 
 /**
- * This is the model class for table "Payment_Record".
+ * This is the model class for table "payment_record".
  *
- * The followings are the available columns in table 'Payment_Record':
+ * The followings are the available columns in table 'payment_record':
  * @property integer $id
  * @property string $order_id
+ * @property string $item
  * @property string $email
  * @property string $date
- * @property string $order_total
+ * @property double $order_total
  * @property string $name
  * @property string $company
+ * @property string $addr1
+ * @property string $addr2
+ * @property string $city
+ * @property string $state
+ * @property string $zip
+ * @property string $country
  * @property string $phone
- * @property string $product_shipping
+ * @property string $product_name
+ * @property double $product_price
+ * @property string $currency_id
+ * @property double $currency_total
  * @property integer $affiliate_id
- * @property string $ship_state
  */
 class PaymentRecord extends CActiveRecord
 {
@@ -34,14 +43,16 @@ class PaymentRecord extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('id, affiliate_id', 'numerical', 'integerOnly'=>true),
-			array('order_id, email, name', 'length', 'max'=>100),
-			array('date, order_total, phone, product_shipping', 'length', 'max'=>50),
-			array('company', 'length', 'max'=>200),
-			array('ship_state', 'length', 'max'=>2),
+			array('affiliate_id', 'numerical', 'integerOnly'=>true),
+			array('order_total, product_price, currency_total', 'numerical'),
+			array('order_id, email, name, city, state', 'length', 'max'=>100),
+			array('item, date, phone', 'length', 'max'=>50),
+			array('company, product_name', 'length', 'max'=>200),
+			array('addr1, addr2', 'length', 'max'=>500),
+			array('zip, country, currency_id', 'length', 'max'=>20),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, order_id, email, date, order_total, name, company, phone, product_shipping, affiliate_id, ship_state', 'safe', 'on'=>'search'),
+			array('id, order_id, item, email, date, order_total, name, company, addr1, addr2, city, state, zip, country, phone, product_name, product_price, currency_id, currency_total, affiliate_id', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -64,15 +75,24 @@ class PaymentRecord extends CActiveRecord
 		return array(
 			'id' => 'Id',
 			'order_id' => 'Order',
+			'item' => 'Item',
 			'email' => 'Email',
 			'date' => 'Date',
 			'order_total' => 'Order Total',
 			'name' => 'Name',
 			'company' => 'Company',
+			'addr1' => 'Addr1',
+			'addr2' => 'Addr2',
+			'city' => 'City',
+			'state' => 'State',
+			'zip' => 'Zip',
+			'country' => 'Country',
 			'phone' => 'Phone',
-			'product_shipping' => 'Product Shipping',
+			'product_name' => 'Product Name',
+			'product_price' => 'Product Price',
+			'currency_id' => 'Currency',
+			'currency_total' => 'Currency Total',
 			'affiliate_id' => 'Affiliate',
-			'ship_state' => 'Ship State',
 		);
 	}
 
@@ -98,32 +118,50 @@ class PaymentRecord extends CActiveRecord
 
 		$criteria->compare('order_id',$this->order_id,true);
 
+		$criteria->compare('item',$this->item,true);
+
 		$criteria->compare('email',$this->email,true);
 
 		$criteria->compare('date',$this->date,true);
 
-		$criteria->compare('order_total',$this->order_total,true);
+		$criteria->compare('order_total',$this->order_total);
 
 		$criteria->compare('name',$this->name,true);
 
 		$criteria->compare('company',$this->company,true);
 
+		$criteria->compare('addr1',$this->addr1,true);
+
+		$criteria->compare('addr2',$this->addr2,true);
+
+		$criteria->compare('city',$this->city,true);
+
+		$criteria->compare('state',$this->state,true);
+
+		$criteria->compare('zip',$this->zip,true);
+
+		$criteria->compare('country',$this->country,true);
+
 		$criteria->compare('phone',$this->phone,true);
 
-		$criteria->compare('product_shipping',$this->product_shipping,true);
+		$criteria->compare('product_name',$this->product_name,true);
+
+		$criteria->compare('product_price',$this->product_price);
+
+		$criteria->compare('currency_id',$this->currency_id,true);
+
+		$criteria->compare('currency_total',$this->currency_total);
 
 		$criteria->compare('affiliate_id',$this->affiliate_id);
 
-		$criteria->compare('ship_state',$this->ship_state,true);
-
-		return new CActiveDataProvider('Payment_Record', array(
+		return new CActiveDataProvider('payment_record', array(
 			'criteria'=>$criteria,
 		));
 	}
 
 	/**
 	 * Returns the static model of the specified AR class.
-	 * @return Payment_Record the static model class
+	 * @return payment_record the static model class
 	 */
 	public static function model($className=__CLASS__)
 	{
--- protected/modules/v1/controllers/PaymentController.php
+++ protected/modules/v1/controllers/PaymentController.php
@@ -2,16 +2,16 @@
 class PaymentController extends ApiController
 {
 	/**
-	 * 
+	 *  {"date":"09\/09\/2015","order_id":"3CYZDNL-AFV4LD","item":"56599-1","quantity":"1","order_total":"50.01","first_name":"jijun","last_name":"zhang","name":"jijun zhang","company":"","addr1":"shuanglin street","addr2":"","city":"chengdu","state":"Sichuan Sheng","zip":"610000","country":"CN","phone":"","email":"jerry.z@wisape.com","link_id":"","has_tax":"0","ship_country":"CN","ship_state":"Sichuan Sheng","product_name":"????1????","product_price":"50","customer_email_opt_in":"","currency_id":"CNY","currency_total":"349","affiliate_id":"618601","coupon_id":"","vendor_id":"56599","cross_sell":""} 
 	 */
 	public function actionMycommerce(){
-// 		$order_id = $_REQUEST['order_id'];
+// 		echo json_encode($_REQUEST);exit;
 		$model = new PaymentRecord;
 		$model->attributes = $_REQUEST;
 		if($model->save()){
-			echo 'success';
+			echo $model->email;
 			exit;
 		}
-		echo 'fail';
+		echo 'error!Please send email to support@wisape.com';
 	}
 }
