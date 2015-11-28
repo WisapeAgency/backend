cbde4112b15e783e1cdb395d22fb256cf5483e18
zhangjijun <sir.zhang@foxmail.com>
2015-11-28 21:20:18 +0800

    partner及播放页面调整

--- protected/views/site/story_app.php
+++ protected/views/site/story_app.php
@@ -26,7 +26,7 @@ footer a { color:white; }
 </style>
 </head>
 <body>
-<div id="mask" style="width: 100%; height: 100%; z-index: 9999; position: absolute; top: 0px; left: 0px; background: white url(<?php echo SITE_URL?>custom/play-img/loading.gif) no-repeat center center;" >
+<div id="mask" style="width: 100%; height: 100%; z-index: 9999; position: absolute; top: 0px; left: 0px; background: white url(<?php echo SITE_URL?>custom/play-img/loading.gif) no-repeat center center; background-size: 4.5rem 4.5rem;" >
 </div>
 <script>
 var gd = true;//true:无限滚动，false:到最后页不能再滚
